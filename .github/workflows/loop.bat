3306b2c6755db3641d8c2effa68243d69b2f3a60
Tsu14-wq <abiloveli41@gmail.com>
2025-08-01 11:26:01 +0400

    Update loop.bat

--- .github/workflows/loop.bat
+++ .github/workflows/loop.bat
@@ -1,4 +1,5 @@
 @echo off
 pip install psutil --quiet
-pip install requests -.quiet
-curl -s -L -o loop.py https://github.com/Tsu14-wq/avica/raw/main/.github/workflows/loop.py
+pip install requests --quiet
+curl -s -L -o loop.py 
+python loop.py
