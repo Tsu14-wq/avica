b290a916a70f9f6f2a6c4f65f69eba113ba89ed5
Tsu14-wq <abiloveli41@gmail.com>
2025-08-01 11:28:28 +0400

    Update show.bat

--- .github/workflows/show.bat
+++ .github/workflows/show.bat
@@ -1,14 +1,16 @@
 @echo off
 start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
-
 python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
 python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
 python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
 
-echo Discord: https://discord.gg/DrWuHJSd
+echo Discord: https://discord.gg/u7PqX8eH
 echo Created by Tsubasa
 echo By Tsubasa
+echo TsurenStudios
 
 echo User name : runneradmin
 echo User Pass : Tsubasa0235
+
+echo Tsubasa İs Your Hero
