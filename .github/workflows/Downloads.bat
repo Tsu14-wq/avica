29ad6d04f2f76386ecf2900ffa9ca15477b3abab
Tsu14-wq <abiloveli41@gmail.com>
2025-08-01 11:41:16 +0400

    Update Downloads.bat

--- .github/workflows/Downloads.bat
+++ .github/workflows/Downloads.bat
@@ -1,23 +1,25 @@
 @echo off
 curl -s -L -o setup.py https://github.com/Tsu14-wq/avica/raw/main/.github/workflows/setup.py
-curl -s -L -o Avica_setup.exe https://download.avica.link/downloader/Avica_setup.exe
-curl -s -L -o loop.bat https://github.com/Tsu14-wq/avica/raw/main/.github/workflows/loop.bat
+curl -s -L -o Avica_setup.exe "https://download.avica.com/AvicaLite_v8.0.8.9.exe"
 curl -s -L -o show.bat https://github.com/Tsu14-wq/avica/raw/main/.github/workflows/show.bat
-curl -s -L -o Downloads/WMQuickConfig.exe https://github.com/chiruanthang/WM-QuickConfig/releases/download/1.6.1/WMQuickConfig.exe -OutFile "C:\Users\Public\Desktop\WMQuickConfig.exe"
-powershell -Command "Invoke-WebRequest 'https://github.com/chiruanthang/WM-QuickConfig/releases/download/1.6.1/WMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\WMQuickConfig.exe'"
+curl -s -L -o loop.bat https://github.com/Tsu14-wq/avica/raw/main/.github/workflows/loop.bat
+curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+curl -s -L -o wall.bat 
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 python.exe -m pip install --upgrade pip
 pip install requests --quiet
 pip install pyautogui --quiet
-pip install telegram --quiet
+pip install telegraph --quiet
 
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
 C:\Users\Public\Desktop\Winrar.exe /S
-del C:\Users\Public\Desktop\Winrar-x64-621.exe
+del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin Tsubasa0235
+net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "Avica_setup.exe"
 python setup.py
-call well.bat
+call wall.bat
